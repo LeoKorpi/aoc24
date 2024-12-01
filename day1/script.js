c4c382038bff873946e9bd80dfa7cb046e105ce6
LeoKorpi <leokorpi.game@gmail.com>
2024-12-01 23:36:17 +0100

    added comments

--- day1/script.js
+++ day1/script.js
@@ -1,15 +1,17 @@
 const fs = require("node:fs");
 let data = "";
 
+// import data from local file
 try {
   data = fs.readFileSync("./day1/input.txt", "utf-8");
 } catch (err) {
   console.log("Error:" + err);
 }
 
+// remove all spaces and breaklines, split into array of numbers
 let dataArray = data.replace(/\s+/g, " ").match(/\d+/g);
-// dataArray = dataArray.replace("\n", "");
 
+// sort both arrays in ascending order
 let leftList = dataArray
   .filter((_, index) => index % 2 === 0)
   .sort((a, b) => a - b);
@@ -17,22 +19,22 @@ let rightList = dataArray
   .filter((_, index) => index % 2 === 1)
   .sort((a, b) => a - b);
 
+// calculate the total distance between all IDs
 let totalDistance = 0;
-
 for (let i = 0; i < leftList.length; i++) {
   totalDistance += Math.abs(rightList[i] - leftList[i]);
 }
 
+// see which values appear in both lists and how many times
 let frequencyMap = rightList.reduce((map, num) => {
   map[num] = (map[num] || 0) + 1;
   return map;
 }, {});
 
+// calculate the similarity score
 let similarityScore = leftList.reduce((score, num) => {
   if (frequencyMap[num]) {
     score += num * frequencyMap[num];
   }
   return score;
 }, 0);
-
-console.log("similarityScore: " + similarityScore);
